c04bf10c1664424a8d31ad3725c90c68d351e307
ksmiththedev13 <152726995+ksmiththedev13@users.noreply.github.com>
2024-06-23 11:38:34 -0500

    Forest big update
    sigma

---
+++ EM/Assets/Scripts/KevinAgreement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using KevinAgreement; 
+
+namespace KevinAgreement
+{
+    public class KevinAgreement : MonoBehaviour
+    {
+        public Transform KevinAgreementTransform;
+    }
+}
